7ec5705a65bb002b494186610e19c1702378c76b
Richard Hatherall <richard@appercept.com>
2022-05-13 15:43:56 +0100

    Add SQS Queue Poller sample

---
+++ SQSQueuePoller/Poller.dpr
@@ -0,0 +1,60 @@
+﻿program Poller;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  AWS.SQS,
+  System.SysUtils;
+
+var
+  QueueURL: string;
+  QueuePoller: ISQSQueuePoller;
+
+const
+  LOG_DATE_FORMAT = 'yyyymmdd''T''hhnnss';
+
+procedure Log(const AMessage: string);
+begin
+  Writeln(Format('[%s] %s', [FormatDateTime(LOG_DATE_FORMAT, Now), AMessage]));
+end;
+
+begin
+  try
+    QueueURL := ParamStr(1);
+    if QueueURL.IsEmpty then
+    begin
+      Writeln('Please specify Queue URL as argument.');
+      Exit;
+    end;
+
+    QueuePoller := TSQSQueuePoller.Create(QueueURL);
+
+    QueuePoller.OnBeforeRequest := procedure(const APoller: ISQSQueuePoller;
+                                             const AStats: TSQSQueuePollerStatistics)
+      begin
+        if AStats.RequestCount = 0 then
+          Log(Format('Polling started at %s', [FormatDateTime('c', AStats.PollingStartedAt)]))
+        else
+          Log(Format('Polling iteration %d', [AStats.RequestCount]));
+        Log(Format('Messages received to date %d', [AStats.ReceivedMessageCount]));
+      end;
+
+    // Uncomment the following line to enable receiving in batches.
+    // QueuePoller.MaxNumberOfMessages := 3;
+
+    QueuePoller.Poll(
+      procedure(const AMessages: TSQSMessages)
+      var
+        LMessage: ISQSMessage;
+      begin
+        for LMessage in AMessages do
+          Log(Format('Received message: %s', [LMessage.Body]));
+      end
+    );
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
---
+++ SQSQueuePoller/Sender.dpr
@@ -0,0 +1,47 @@
+﻿program Sender;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  AWS.SQS,
+  System.SysUtils;
+
+var
+  SQS: TSQSClient;
+  SenderName: string;
+  QueueURL: string;
+  MessageCount: Integer;
+
+begin
+  try
+    SenderName := ParamStr(1);
+    QueueURL := ParamStr(2);
+
+    if SenderName.IsEmpty or QueueURL.IsEmpty then
+    begin
+      Writeln('Please specify sender name and Queue URL as arguments.');
+      Exit;
+    end;
+
+    SQS := TSQSClient.Create;
+    try
+      MessageCount := 0;
+      while True do
+      begin
+        Inc(MessageCount);
+        SQS.SendMessage(
+          QueueURL,
+          Format('Message %d from %s', [MessageCount, SenderName])
+        );
+        Sleep(1000);
+      end;
+    finally
+      SQS.Free;
+    end;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
